c43510645711f9ea583a21db34c5c31a731fef8c
penny <a2485965ufeh8pyoh4dk@gmail.com>
2023-03-10 17:59:40 +0800

    add search function

--- app.js
+++ app.js
@@ -15,14 +15,32 @@ app.set('view engine', 'handlebars')
 // Setting static file
 app.use(express.static('public'))
 
-
 // Setting the route and corresponding response
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     res.render('index', { restaurantsData })
+})
+
+app.get('/restaurants/:id', (req, res) => {
+    const { id } = req.params // params型別為字串
+    const restaurantInfo = restaurantsData.find(
+            data => data.id === Number(id) // 將字串型別的id轉為數字型別
+        )
+    res.render('show', { restaurantInfo })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantsData.filter(
+    data => 
+      data.name.toLowerCase().includes(keyword) ||
+      data.category.includes(keyword)
+
+  )
+  res.render('index', { restaurantsData: restaurants, keyword})
 })
 
 
 // Listen the server when it started
-app.listen(port,()=>{
+app.listen( port, () => {
     console.log(`Express is running on http://localhost:${port}`)
 })
